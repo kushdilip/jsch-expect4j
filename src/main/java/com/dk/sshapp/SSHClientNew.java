8a7f573e26c0af1557fcc62fb230aeae9f485e42
dilip kushwaha <kush.dilip@gmail.com>
2013-01-24 18:52:07 +0530

    Everything working in new SSHClient java class, no need to add Thread.sleep

--- src/main/java/com/dk/sshapp/SSHClientNew.java
+++ src/main/java/com/dk/sshapp/SSHClientNew.java
@@ -13,124 +13,137 @@ import com.jcraft.jsch.Session;
 import expect4j.Closure;
 import expect4j.Expect4j;
 import expect4j.ExpectState;
+import expect4j.matches.EofMatch;
 import expect4j.matches.Match;
 import expect4j.matches.RegExpMatch;
+import expect4j.matches.TimeoutMatch;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class SSHClient 
-{
+public class SSHClientNew {
 	private static final int COMMAND_EXECUTION_SUCCESS_OPCODE = -2;
 	private static String ENTER_CHARACTER = "\r";
 	private static final int SSH_PORT = 22;
 	private List<String> lstCmds = new ArrayList<String>();
-	private static String[] linuxPromptRegEx = new String[]{"/home/sshtest#",
-		"$", "~$","Password:",
-		"[sudo] password for sshtest:",
-		"Do you want to continue [Y/n]?"
-		};
-	
+	private static String[] linuxPromptRegEx = new String[] { "/home/sshtest#",
+			"$", "~$", "Password:", "[sudo] password for sshtest:",
+			"Do you want to continue [Y/n]?" };
+
 	private static String rootPassword = "kush.1dilip";
-	
+
 	private Expect4j expect = null;
 	private StringBuilder buffer = new StringBuilder();
 	private String userName;
 	private String password;
 	private String host;
-	
+	private long defaultTimeOut = Expect4j.TIMEOUT_DEFAULT;
 	private String tempBuffer;
+
 	/**
-	 *
+	 * 
 	 * @param host
 	 * @param userName
 	 * @param password
 	 */
-	public SSHClient(String host, String userName, String password) {
+	public SSHClientNew(String host, String userName, String password) {
 		super();
 		this.userName = userName;
 		this.password = password;
 		this.host = host;
 	}
-	
-	public String execute(List<String> cmdsToExecute){
+
+	public String execute(List<String> cmdsToExecute) {
 		this.lstCmds = cmdsToExecute;
-		
+		boolean isSuccess = true;
+
 		Closure closure = new Closure() {
 			public void run(ExpectState expectState) throws Exception {
 				tempBuffer = expectState.getBuffer();
+				// System.out.println(tempBuffer);
 				buffer.append(tempBuffer);
+				expectState.exp_continue();
 			}
 		};
-		List<Match> lstPattern =  new ArrayList<Match>();
-		for (String regexElement : linuxPromptRegEx) {
-			try {
-				Match mat = new RegExpMatch(regexElement, closure);
-				lstPattern.add(mat);
-			} catch (MalformedPatternException e) {
-				e.printStackTrace();
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		
-		try {
-			
-			expect = SSH();
-			
-			boolean isSuccess = true;
-			for(String strCmd : lstCmds) {
-				isSuccess = isSuccess(lstPattern,strCmd,2000);
-				if (!isSuccess) {
-					isSuccess = isSuccess(lstPattern,strCmd,2000);
+		List<Match> lstPattern = new ArrayList<Match>();
+		synchronized (lstPattern) {
+			for (String regexElement : linuxPromptRegEx) {
+				try {
+					Match mat = new RegExpMatch(regexElement, closure);
+					lstPattern.add(mat);
+				} catch (MalformedPatternException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
-			
-			//root login
-			System.out.println("root login:" + isSuccess(lstPattern, "su root",2000));
-			System.out.println(isSuccess(lstPattern, rootPassword,2000));
+			lstPattern.add(new EofMatch(new Closure() {
+				public void run(ExpectState state) throws Exception {
+				}
+			}));
+			lstPattern.add(new TimeoutMatch(defaultTimeOut, new Closure() {
+				public void run(ExpectState state) {
+				}
+			}));
+		}
 
-			//installing synaptic
-			System.out.println("synaptic install :" + isSuccess(lstPattern,"sudo apt-get install bluefish",2000));
-//			System.out.println(isSuccess(lstPattern,"Y",12000));
-			System.out.println("exit: " + isSuccess(lstPattern, "exit",2000));
+		try {
+
+			expect = SSH();
+			expect.setDefaultTimeout(defaultTimeOut);
+
+			for (String strCmd : lstCmds) {
+				if (isSuccess) {
+					isSuccess = isSuccess(lstPattern, strCmd, 2000);
+				}
+			}
+
+			// root login
+			System.out.println("root login:"
+					+ isSuccess(lstPattern, "su root", 2000));
+			System.out.println(isSuccess(lstPattern, rootPassword, 2000));
+
+			// installing synaptic
+			System.out.println("synaptic install :"
+					+ isSuccess(lstPattern, "apt-get install bluefish",
+							2000));
+			System.out.println(isSuccess(lstPattern, "Y", 12000));
+			System.out.println("exit: " + isSuccess(lstPattern, "exit", 2000));
 			System.out.println("finally: " + expect.expect("~$"));
-			
-			
+
 			checkResult(expect.expect(lstPattern));
-			
-			
-			
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
 			closeConnection();
 		}
 		return buffer.toString();
-		
+
 	}
-	
+
 	/**
-	 *
+	 * 
 	 * @param objPattern
 	 * @param strCommandPattern
 	 * @return
 	 */
-	private boolean isSuccess(List<Match> objPattern,String strCommandPattern,int sleepTime) {
+	private boolean isSuccess(List<Match> objPattern, String strCommandPattern,
+			int sleepTime) {
 		sleepTime = 1000;
 		try {
 			boolean isFailed = checkResult(expect.expect(objPattern));
-			System.out.println("tempBuffer: " + tempBuffer);
+
 			if (!isFailed) {
 				expect.send(strCommandPattern);
 				expect.send(ENTER_CHARACTER);
-				
-//				System.out.println("BEFORE SLEEP, " + strCommandPattern + ": " + expect.expect(objPattern));
-				
-				Thread.sleep(sleepTime);
-				
+
+				// System.out.println("BEFORE SLEEP, " + strCommandPattern +
+				// ": " + expect.expect(objPattern));
+
+				// Thread.sleep(sleepTime);
+
 				return true;
 			}
 			return false;
@@ -142,8 +155,9 @@ public class SSHClient
 			return false;
 		}
 	}
+
 	/**
-	 *
+	 * 
 	 * @param hostname
 	 * @param username
 	 * @param password
@@ -157,18 +171,20 @@ public class SSHClient
 		if (password != null) {
 			session.setPassword(password);
 		}
-		
-		Hashtable<String,String> config = new Hashtable<String,String>();
+
+		Hashtable<String, String> config = new Hashtable<String, String>();
 		config.put("StrictHostKeyChecking", "no");
 		session.setConfig(config);
 		session.connect(60000);
 		ChannelShell channel = (ChannelShell) session.openChannel("shell");
-		Expect4j expect = new Expect4j(channel.getInputStream(), channel.getOutputStream());
+		Expect4j expect = new Expect4j(channel.getInputStream(),
+				channel.getOutputStream());
 		channel.connect();
 		return expect;
 	}
+
 	/**
-	 *
+	 * 
 	 * @param intRetVal
 	 * @return
 	 */
@@ -178,20 +194,23 @@ public class SSHClient
 		}
 		return false;
 	}
+
 	/**
 	 *
 	 */
 	private void closeConnection() {
-		if (expect!=null) {
+		if (expect != null) {
 			expect.close();
 		}
 	}
+
 	/**
-	 *
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SSHClient ssh = new SSHClient("192.168.56.101", "sshtest", "sshtest");
+		SSHClientNew ssh = new SSHClientNew("192.168.56.101", "sshtest",
+				"sshtest");
 		List<String> cmdsToExecute = new ArrayList<String>();
 		cmdsToExecute.add("ls");
 		cmdsToExecute.add("pwd");
